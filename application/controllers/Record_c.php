e57880352060b0c42adffc7b4f6fe833a34d446b
ivan006 <ivan.copeland2015@gmail.com>
2020-12-19 10:17:48 +0200

    1

--- application/controllers/Record_c.php
+++ application/controllers/Record_c.php
@@ -37,143 +37,136 @@ class Record_c extends CI_Controller
 
 		$record = $this->table_page_lib->fetch_for_record($table, "id", $record_id)["posts"][0];
 
-		$haystack = "id";
-		$needle = $record_id;
 
-		$data["rec_o"]["tab_o"]["data_endpoint"] = "fetch_for_record/h/$haystack/n/$needle";
-		$data["rec_o"]["tab_o"]["type"] = "overview";
-		$data["rec_o"]["tab_o"]["rel_name"] = "overview";
-		$data["rec_o"]["tab_o"]["rel_name_id"] = $data["rec_o"]["tab_o"]["rel_name"];
-		$data["rec_o"]["tab_o"]["table"] = $table;
-		$dont_scan = "";
+		// echo $record;
+		if (!empty($record)) {
+			$erd_path = APPPATH.'erd/erd/erd.json';
+			$erd = file_get_contents($erd_path);
+			$erd = json_decode($erd, true);
 
 
-		$erd_path = APPPATH.'erd/erd/erd.json';
-		$erd = file_get_contents($erd_path);
-		$erd = json_decode($erd, true);
+			$dont_scan = "";
 
-		$data["rec_o"]["tab_d"]["cols"]["editable"] = $erd[$table]["fields"];
+			$rec_o = $this->table_o_and_d("overview", $erd, $table, null, $record["id"], $dont_scan);
 
 
-
-		$record_inherited_cols = $this->table_page_lib->record_inherited_cols($data["rec_o"]["tab_o"]["table"], $erd);
-
-		$data["rec_o"]["tab_d"]["cols"]["visible"] = array();
-
-		$data["rec_o"]["tab_d"]["cols"]["visible"] = $record_inherited_cols["self"];
-
-		// header('Content-Type: application/json');
-		// echo json_encode($record_inherited_cols, JSON_PRETTY_PRINT);
-		// exit;
-
-		foreach ($record_inherited_cols["rel"] as $key => $value) {
+			$rec_d = array();
+			if (isset($erd[$rec_o["tab_o"]["table"]]["items"])) {
+				$items = $erd[$rec_o["tab_o"]["table"]]["items"];
+				foreach ($items as $key => $value) {
+					if ($key !== $dont_scan) {
 
 
+						$rec_d[$key] = $this->table_o_and_d("details", $erd, $key, $value, $record["id"], $dont_scan);
 
-			// unset($data["rec_o"]["tab_d"]["cols"]["visible"][$key]);
-			foreach ($value["inherited_cols"] as $key_2 => $value_2) {
-				$cols_wth_props[$key_2] = $value_2["col_props"];
+					} else {
+						$rec_d[$key] = array();
+					}
+				}
 			}
-			$data["rec_o"]["tab_d"]["cols"]["visible"] = array_merge(
-				$data["rec_o"]["tab_d"]["cols"]["visible"],
-				$cols_wth_props
-			);
+
+			// $data["rec_o"]["tab_o"] = $rec_o["tab_o"];
+			// $data["rec_o"]["tab_d"] = $rec_o_tab_d;
+			$data["rec_o"] = $rec_o;
+			$data["rec_d"] = $rec_d;
+
+			// header('Content-Type: application/json');
+			// echo json_encode($record_inherited_cols, JSON_PRETTY_PRINT);
+			// exit;
+
+
+
+
+
+			$this->load->view('table_header_v', array("data"=>$data));
+			$this->load->view('table_block_v', array("data"=>$data["rec_o"]));
+
+			foreach ($data["rec_d"] as $key => $value) {
+				if (!empty($value)) {
+					// code...
+					$this->load->view('table_block_v', array("data"=>$value));
+				}
+			}
+			$this->load->view('table_footer_v', array("data"=>$data));
+
+		} else {
+			return "no such record";
 		}
 
-		// header('Content-Type: application/json');
-		// echo json_encode($data["rec_o"]["tab_d"]["cols"]["visible"], JSON_PRETTY_PRINT);
-		// exit;
-
-		// $data["rec_o"]["tab_d"]["cols"]["visible"] = $record_inherited_cols;
-
-
-		$data["rec_d"] = $this->relations($erd, $data["rec_o"]["tab_o"], $record, $dont_scan);
-
-		// header('Content-Type: application/json');
-		// echo json_encode($record_inherited_cols, JSON_PRETTY_PRINT);
-		// exit;
-
-
-
-
-
-		$this->load->view('table_header_v', array("data"=>$data));
-		$this->load->view('table_block_v', array("data"=>$data["rec_o"]));
-
-		foreach ($data["rec_d"] as $key => $value) {
-			if (!empty($value)) {
-				// code...
-				$this->load->view('table_block_v', array("data"=>$value));
-			}
-		}
-		$this->load->view('table_footer_v', array("data"=>$data));
 
 	}
 
 
-	public function relations($erd, $parent_tab_o, $parent_record, $dont_scan)
+	public function table_o_and_d($rec_part, $erd, $table, $foreign_k, $record_id, $dont_scan)
 	{
-		if (!$this->ion_auth->logged_in())
-		{
-			// redirect them to the login page
-			redirect('auth/login', 'refresh');
+		// if (!$this->ion_auth->logged_in())
+		// {
+		// 	// redirect them to the login page
+		// 	redirect('auth/login', 'refresh');
+		// }
+		if ($rec_part=="overview") {
+
+			$haystack = "id";
+			$needle = $record_id;
+
+			$tab_o["data_endpoint"] = "fetch_for_record/h/$haystack/n/$needle";
+			$tab_o["type"] = "overview";
+			$tab_o["rel_name"] = "overview";
+			$tab_o["rel_name_id"] = $tab_o["rel_name"];
+			$tab_o["table"] = $table;
+
+		} elseif ($rec_part=="details") {
+
+			$haystack = $foreign_k; //changes
+			$needle = $record_id;
+
+			$data_endpoint = "fetch_for_record/h/$haystack/n/$needle";
+
+
+			$tab_o["data_endpoint"] = $data_endpoint;
+			$tab_o["type"] = "dedicated_items"; // changes
+			$tab_o["rel_name"] = $table." (".$foreign_k." specialised)"; // changes
+			$tab_o["rel_name_id"] = preg_replace('/\W+/','',strtolower(strip_tags($tab_o["rel_name"])));
+			$tab_o["table"] = $table; // dynamic
+			// $tab_o["foreign_key"] = $foreign_k;
+
+			// var_dump($parent_tab_o);
 		}
-		$result = array();
-		// $cols = $erd[$parent_tab_o["table"]]["fields"];
-		if (isset($erd[$parent_tab_o["table"]]["items"])) {
-			$items = $erd[$parent_tab_o["table"]]["items"];
-			foreach ($items as $key => $value) {
-				if ($key !== $dont_scan) {
-
-
-					$tab_o["rel_name"] = $key." (".$value." specialised)";
-					$tab_o["rel_name_id"] = preg_replace('/\W+/','',strtolower(strip_tags($tab_o["rel_name"])));
-					$tab_o["table"] = $key;
-					$tab_o["foreign_key"] = $value;
-
-					// var_dump($parent_tab_o);
-
-					if (!empty($parent_record)) {
-
-						$haystack = $value;
-						$needle = $parent_record["id"];
-
-						$data_endpoint = "fetch_for_record/h/$haystack/n/$needle";
-
-					} else {
-						$data_endpoint = "";
-					}
-
-					$tab_o["data_endpoint"] = $data_endpoint;
-
-					$tab_o["type"] = "dedicated_items";
 
 
 
 
 
 
-					$sub_cols = $erd[$key]["fields"];
+		if ($rec_part=="overview") {
 
+			$tab_d["cols"]["editable"] = $erd[$table]["fields"];
 
-					$result[$key] = array(
-						"tab_o" => $tab_o,
-						"tab_d" => array(
-							"cols" => array(
-								"editable" => $sub_cols,
-								"visible" => $sub_cols,
-							),
-						)
-					);
+			$record_inherited_cols = $this->table_page_lib->record_inherited_cols($tab_o["table"], $erd);
 
+			$tab_d["cols"]["visible"] = array();
 
+			$tab_d["cols"]["visible"] = $record_inherited_cols["self"];
 
-				} else {
-					$result[$key] = array();
+			foreach ($record_inherited_cols["rel"] as $key => $value) {
+				foreach ($value["inherited_cols"] as $key_2 => $value_2) {
+					$cols_wth_props[$key_2] = $value_2["col_props"];
 				}
+				$tab_d["cols"]["visible"] = array_merge(
+					$tab_d["cols"]["visible"],
+					$cols_wth_props
+				);
 			}
+		} elseif ($rec_part=="details") {
+
+			$tab_d["cols"]["editable"] = $erd[$table]["fields"];
+			$tab_d["cols"]["visible"] = $erd[$table]["fields"];
 		}
 
+
+
+		$result["tab_o"] = $tab_o;
+		$result["tab_d"] = $tab_d;
 
 
 		return $result;
