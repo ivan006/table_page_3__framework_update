a40839c0c7b643b91ac230eba54f2635b5b3acfd
ivan006 <ivan.copeland2015@gmail.com>
2021-03-06 21:33:33 +0200

    1

--- application/views/table_block_v.php
+++ application/views/table_block_v.php
@@ -602,75 +602,78 @@ $this->load->view('table_scripts_add_v', array(
 
   });
 
-  // Edit Record
-
-
-  <?php
-  $action_type = "edit";
-  ?>
-  $(document).on("click", "#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"]); ?>_edit", function(e){
-    e.preventDefault();
-
-    var edit_id = $(this).attr("value");
 
 
 
-    $.ajax({
-      url: "<?php echo base_url(); ?>api/table/t/<?php echo $data["g_identity"]["g_from"]; ?>/edit",
-      type: "post",
-      dataType: "json",
-      data: {
-        edit_id: edit_id
-      },
-      success: function(data){
-        if (data.responce == "success") {
-          $('#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_" ?>modal').modal('show');
-          $("#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_" ?>record_id").val(data.post.id);
+</script>
+<?php
+// Edit Record
+$action_type = "edit";
+?>
+<script type="text/javascript">
+
+
+$(document).on("click", "#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"]); ?>_edit", function(e){
+  e.preventDefault();
+
+  var edit_id = $(this).attr("value");
+
+
+
+  $.ajax({
+    url: "<?php echo base_url(); ?>api/table/t/<?php echo $data["g_identity"]["g_from"]; ?>/edit",
+    type: "post",
+    dataType: "json",
+    data: {
+      edit_id: edit_id
+    },
+    success: function(data){
+      if (data.responce == "success") {
+        $('#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_" ?>modal').modal('show');
+        $("#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_" ?>record_id").val(data.post.id);
+        <?php
+        foreach ($editable_rows as $key => $value) {
+          ?>
+
           <?php
-          foreach ($editable_rows as $key => $value) {
+          if ($key !== "id") {
             ?>
 
+            var g_ability_html_id_edit = $("#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key); ?>");
+
             <?php
-            if ($key !== "id") {
+            if (isset($value["assumable"])) {
               ?>
 
-              var g_ability_html_id_edit = $("#<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key); ?>");
-
-              <?php
-              if (isset($value["assumable"])) {
-                ?>
-
-                state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"] = <?php echo $data["g_identity"]["g_where_needle"] ?>;
-
-                <?php
-              }
-              else {
-                ?>
-
-                state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"] = data.post["<?php echo $key; ?>"];
-
-                <?php
-              }
-              ?>
-
-              g_ability_html_id_edit.val(state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"]);
-              // alert(state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"]);
-
-              // alert(JSON.stringify(state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"]));
+              state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"] = <?php echo $data["g_identity"]["g_where_needle"] ?>;
 
               <?php
             }
-          }
-          ?>
-        }else{
-          toastr["error"](data.message);
-        }
-      }
-    });
+            else {
+              ?>
 
+              state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"] = data.post["<?php echo $key; ?>"];
+
+              <?php
+            }
+            ?>
+
+            g_ability_html_id_edit.val(state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"]);
+            // alert(state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"]);
+
+            // alert(JSON.stringify(state["<?php echo makeSafeForCSS($data["g_identity"]["g_ability_html_id"])."_".$action_type."_".makeSafeForCSS($key)."_"."value" ?>"]));
+
+            <?php
+          }
+        }
+        ?>
+      }else{
+        toastr["error"](data.message);
+      }
+    }
   });
 
-
+});
 
 </script>
 
---
+++ application/views/table_scripts_edit_v.php
@@ -0,0 +1,76 @@
+<?php
+$editable_rows = $data["g_select"]["editable"];
+$readable_rows = $data["g_select"]["visible"];
+
+$safe_ability_id = makeSafeForCSS($data["g_identity"]["g_ability_html_id"]);
+$safe_ability_action_id = $safe_ability_id."_".$action_type."_";
+?>
+<script type="text/javascript">
+
+$(document).on("click", "#<?php echo $safe_ability_id; ?>_edit", function(e){
+  e.preventDefault();
+
+  var edit_id = $(this).attr("value");
+
+
+
+  $.ajax({
+    url: "<?php echo base_url(); ?>api/table/t/<?php echo $data["g_identity"]["g_from"]; ?>/edit",
+    type: "post",
+    dataType: "json",
+    data: {
+      edit_id: edit_id
+    },
+    success: function(data){
+      if (data.responce == "success") {
+        $('#<?php echo $safe_ability_action_id ?>modal').modal('show');
+        $("#<?php echo $safe_ability_action_id ?>record_id").val(data.post.id);
+        <?php
+        foreach ($editable_rows as $key => $value) {
+          $key_safe = makeSafeForCSS($key);
+          ?>
+
+          <?php
+          if ($key !== "id") {
+            ?>
+
+            var g_ability_html_id_edit = $("#<?php echo $safe_ability_action_id.$key_safe; ?>");
+
+            <?php
+            if (isset($value["assumable"])) {
+              ?>
+
+              state["<?php echo $safe_ability_action_id.$key_safe."_"."value" ?>"] = <?php echo $data["g_identity"]["g_where_needle"] ?>;
+
+              <?php
+            }
+            else {
+              ?>
+
+              state["<?php echo $safe_ability_action_id.$key_safe."_"."value" ?>"] = data.post["<?php echo $key; ?>"];
+
+              <?php
+            }
+            ?>
+
+            g_ability_html_id_edit.val(
+              state["<?php echo $safe_ability_action_id.$key_safe."_"."value" ?>"]
+            );
+
+            // alert(state["<?php echo $safe_ability_action_id.$key_safe."_"."value" ?>"]);
+
+            // alert(JSON.stringify(state["<?php echo $safe_ability_action_id.$key_safe."_"."value" ?>"]));
+
+            <?php
+          }
+        }
+        ?>
+      }else{
+        toastr["error"](data.message);
+      }
+    }
+  });
+
+});
+
+</script>
