4b892e6be828f8756dd02d0feaff5d0ca4640143
ivan006 <ivan.copeland2015@gmail.com>
2021-05-15 14:33:24 +0200

    1

--- application/sql/3/activity_log/activity_log.sql
+++ application/sql/3/activity_log/activity_log.sql
@@ -8,8 +8,8 @@ CREATE TABLE `_activity_log` (
   `record_table_and_id` varchar(255) NOT NULL,
   `timestamp` datetime NOT NULL,
   `last_activity_type` varchar(255) NOT NULL,
-  `owner` int(11) NOT NULL,
-  `editability` int(11) NOT NULL,
-  `visibility` int(11) NOT NULL,
+  `owner` int(11) NOT NULL DEFAULT 1,
+  `editability` int(11) NOT NULL DEFAULT 1,
+  `visibility` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`record_table_and_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
